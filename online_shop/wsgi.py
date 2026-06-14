d8a87b91f357da0a6f5bb34f62bfacaf1cbfd645
Temiraliyev <156788797+temiraliyevm@users.noreply.github.com>
2026-06-15 00:03:20 +0500

    ../

--- online_shop/wsgi.py
+++ online_shop/wsgi.py
@@ -5,7 +5,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
-# create user with 'manager' role
-# create_manager()
 
 application = get_wsgi_application()
