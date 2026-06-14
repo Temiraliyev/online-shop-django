3646a112ca6ea738d24b9ccaa777ccb84b2123c2
Temiraliyev <156788797+temiraliyevm@users.noreply.github.com>
2026-06-14 23:56:42 +0500

    ../

--- online_shop/wsgi.py
+++ online_shop/wsgi.py
@@ -2,7 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from accounts.views import create_manager
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
 
